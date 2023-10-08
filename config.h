fe9a96ebf6c92733cfdcaf786636b94c7591f873
Arthur <me@wzray.ru>
2023-10-08 17:09:33 +0300

    update keybinds

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* appearance */
 static const unsigned int borderpx    = 2;
-static const unsigned int snap        = 32;
+static const unsigned int snap        = 0;
 static const unsigned int gappih      = 20;
 static const unsigned int gappiv      = 10;
 static const unsigned int gappoh      = 10;
@@ -64,8 +64,8 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,		KEY,      tagview,        {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,		KEY,      tagview,        {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|Mod1Mask,		KEY,      toggletag,      {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggleview,     {.ui = 1 << TAG} }, \
 
@@ -78,67 +78,68 @@ static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
 static const Key keys[] = {
-	/* modifier         key        function        argument */
+	/* modifier           key        function        argument */
 	// movement
-	{ MODKEY,           XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,           XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,           XK_b,      togglebar,      {0} },
-	{ MODKEY,           XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,           XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY|ShiftMask, XK_l,      incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask, XK_h,      incnmaster,     {.i = -1 } },
-	{ MODKEY,           XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,           XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,           XK_c,      zoom,           {0} },
-	{ MODKEY,           XK_Tab,    view,           {0} },
-	{ MODKEY,           XK_q,      killclient,     {0} },
+	{ MODKEY,             XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,             XK_b,      togglebar,      {0} },
+	{ MODKEY,             XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,             XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,   XK_l,      incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,   XK_h,      incnmaster,     {.i = -1 } },
+	{ MODKEY,             XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,             XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,             XK_c,      zoom,           {0} },
+	{ MODKEY,             XK_Tab,    view,           {0} },
+	{ MODKEY,             XK_q,      killclient,     {0} },
 
 	// layout
-	{ MODKEY,           XK_y,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,           XK_u,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,           XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,           XK_f,      togglefullscr,  {0} },
-	{ MODKEY|ShiftMask, XK_f,      togglefloating, {0} },
-	{ MODKEY|ShiftMask, XK_j,      movestack,      {.i = +1 } },
-	{ MODKEY|ShiftMask, XK_k,      movestack,      {.i = -1 } },
-	{ MODKEY,           XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask, XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,           XK_p,      invertdir,      {0} },
+	{ MODKEY,             XK_y,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,             XK_u,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,             XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,             XK_f,      togglefullscr,  {0} },
+	{ MODKEY|ShiftMask,   XK_f,      togglefloating, {0} },
+	{ MODKEY|ShiftMask,   XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,   XK_k,      movestack,      {.i = -1 } },
+	{ MODKEY,             XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,   XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,             XK_p,      invertdir,      {0} },
 
 	// apps
-	{ MODKEY,           XK_w,      spawn,          SHCMD("firefox") },
-	{ MODKEY,           XK_t,      spawn,          SHCMD("telegram-desktop") },
-	{ MODKEY|ShiftMask, XK_s,      spawn,          SHCMD("flameshot", "gui") },
-	{ MODKEY,           XK_i,      spawn,          SHCMD("dunstctl", "history-pop") },
-	{ MODKEY,           XK_o,      spawn,          SHCMD("dunstctl", "close") },
+	{ MODKEY,             XK_w,      spawn,          SHCMD("firefox") },
+	{ MODKEY,             XK_t,      spawn,          SHCMD("telegram-desktop") },
+	{ MODKEY|ShiftMask,   XK_s,      spawn,          SHCMD("flameshot", "gui") },
+	{ MODKEY,             XK_i,      spawn,          SHCMD("dunstctl", "history-pop") },
+	{ MODKEY,             XK_o,      spawn,          SHCMD("dunstctl", "close") },
+	{ MODKEY,             XK_x,      spawn,          SHCMD("discord") },
 
 	// tags
-	TAGKEYS(            XK_1,                      0)
-	TAGKEYS(            XK_2,                      1)
-	TAGKEYS(            XK_3,                      2)
-	TAGKEYS(            XK_4,                      3)
-	TAGKEYS(            XK_5,                      4)
-	TAGKEYS(            XK_6,                      5)
-	TAGKEYS(            XK_7,                      6)
-	TAGKEYS(            XK_8,                      7)
-	TAGKEYS(            XK_9,                      8)
-	{ MODKEY|ShiftMask, XK_q,      quit,           {0} },
+	TAGKEYS(              XK_1,                      0)
+	TAGKEYS(              XK_2,                      1)
+	TAGKEYS(              XK_3,                      2)
+	TAGKEYS(              XK_4,                      3)
+	TAGKEYS(              XK_5,                      4)
+	TAGKEYS(              XK_6,                      5)
+	TAGKEYS(              XK_7,                      6)
+	TAGKEYS(              XK_8,                      7)
+	TAGKEYS(              XK_9,                      8)
+	{ MODKEY|ShiftMask,   XK_q,      quit,           {0} },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-	/* click            event mask      button          function        argument */
-	{ ClkLtSymbol,      0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,      0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,      0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,    0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,     MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,     MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,     MODKEY,         Button3,        resizemouse,    {0} }, 
-	{ ClkTagBar,        0,              Button1,        view,           {0} },
-	{ ClkTagBar,        0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,        MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,        MODKEY,         Button3,        toggletag,      {0} },
+	/* click              event mask      button          function        argument */
+	{ ClkLtSymbol,        0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,        0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,        0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,      0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,       MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,       MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,       MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,          0,              Button1,        view,           {0} },
+	{ ClkTagBar,          0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,          MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,          MODKEY,         Button3,        toggletag,      {0} },
 };
 
